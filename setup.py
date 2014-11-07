3e3f2708832f3219d06ccd02e79291d9535d0128
Takeru Ichii <takeru.ichii@facebook.com>
2014-11-07 16:52:17 +0900

    version up

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name          = 'pyIncomingSlack'
-    ,version      = '0.1.1'
+    ,version      = '0.1.2'
     ,description  = 'Post to Slack Channel/Groups using "Incoming WebHooks"'
     ,license      = 'MIT'
     ,author       = 'Takeru Ichii'
