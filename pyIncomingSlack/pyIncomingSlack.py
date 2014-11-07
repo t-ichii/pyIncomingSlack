687e45dfa028462c7475c578456eb9e393b0eab0
Takeru Ichii <takeru.ichii@facebook.com>
2014-11-07 16:34:09 +0900

    Correction install script

--- pyIncomingSlack/pyIncomingSlack.py
+++ pyIncomingSlack/pyIncomingSlack.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__author__ = 'Takeru Ichii'
+
 import requests
 import json
 import argparse
--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@ from setuptools import setup, find_packages
 
 setup(
     name          = 'pyIncomingSlack'
-    ,version      = '0.1'
+    ,version      = '0.1.1'
     ,description  = 'Post to Slack Channel/Groups using "Incoming WebHooks"'
+    ,license      = 'MIT'
     ,author       = 'Takeru Ichii'
     ,author_email = 'takeru.ichii@facebook.com'
     ,url          = 'https://github.com/t-ichii/pyIncomingSlack'
-    ,packages     = ['pyIncomingSlack']
+    ,py_modules   = ['pyIncomingSlack']
+    ,package_dir  = {'' : 'pyIncomingSlack'}
     ,entry_points = {
         "console_scripts": [
-            "pyIncomingSlack=pyIncomingSlack.pyIncomingSlack:main"
+            "pyIncomingSlack=pyIncomingSlack:main"
         ]
     }
 )
